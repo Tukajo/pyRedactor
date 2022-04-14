d1d5247948c1fd8b9b70822fbd33a548cfb8f57d
Timothy Frisch <Tjfrisch@gmail.com>
2022-04-14 10:12:20 -0500

    -- Changes for handling replace inplace.

--- substitutor.py
+++ substitutor.py
@@ -6,7 +6,6 @@ import re
 import fileinput
 
 actionAudits = []
-totalReplacementCount = 0
 common_excluded_paths = ['venv', '.idea', 'node_modules', 'bower_components', 'dist', 'build', '__pycache__', 'git',
                          '.git', 'Python']
 common_excluded_files = ['audits.txt', 'substitute.py', 'substitutor.py', 'substitutor.pyc', 'substitutor.pyw',
@@ -18,6 +17,57 @@ startTime = int(round(datetime.datetime.now().timestamp()) * 1000)
 auditFileName = f'Substitor_Audit_{startTime}.txt'
 
 
+# Main functions
+def searchAndReplaceInFiles(keyword, substitute, baseDirectory, directoryExclusions):
+    filesToBeEdited = findAllFilesContainingKeyword(keyword, baseDirectory, directoryExclusions)
+    with fileinput.FileInput(files=filesToBeEdited, inplace=True) as file:
+        print(f'Processing files: {file.filename}')
+        for line in file:
+            line = line.replace(keyword, substitute)
+            print(line, end='')
+        file = file.nextfile()
+    audit(f'{len(filesToBeEdited)} files were edited.')
+    audit(filesToBeEdited)
+    filesToBeEdited.clear()
+    writeAuditsToFile()
+
+
+def findAllFilesContainingKeyword(keyword, baseDirectory, directoryExclusions):
+    """
+    Finds all files containing the key word.
+    """
+    filesToChange = []
+    keywordRegex = re.compile(re.escape(keyword))
+    for root, dirs, files in os.walk(baseDirectory, topdown=True, followlinks=False):
+        for directory in directoryExclusions:
+            if directory in dirs:
+                audit(f'Skipping excluded directory: {directory}')
+                dirs.remove(directory)
+        for file in files:
+            if getFileName(file) in common_excluded_files:
+                audit(f'Skipping excluded file: {file}')
+                continue
+            else:
+                filePath = os.path.join(root, file)
+                audit(f'Checking file: {filePath}')
+                # try to read file, skip if not readable
+                try:
+                    with fileinput.FileInput(filePath, inplace=False) as f:
+                        for line in f:
+                            anyMatch = re.search(keywordRegex, line)
+                            if anyMatch:
+                                filesToChange.append(filePath)
+                                break
+                    f.close()
+                except Exception as readError:
+                    audit(f'Skipping file, unreadable: {filePath}')
+                    audit(f'Exception: {formatErrorWithTrace(readError)}')
+                    continue
+                writeAuditsToFile()
+    return filesToChange
+
+
+# Audit functions
 def audit(action):
     """
     Audits the actions taken by this script.
@@ -38,81 +88,12 @@ def writeAuditsToFile():
     f.close()
 
 
-def askForBaseDirectory():
+# Utility functions
+def isSymLink(path):
     """
-    Asks the user for the base directory.
+    Checks if the path is a symbolic link.
     """
-    baseDirectory = input("Enter the base directory: \n")
-    return baseDirectory
-
-
-def askForKeyWords():
-    """
-    Asks the user for the keywords.
-    """
-    keyWords = input("Enter the keyword(s) to replace (3 or less): \n")
-    return keyWords
-
-
-def askForSubstitutes():
-    """
-    Asks the user for the substitute for the corresponding key words.
-    """
-    substitutions = input("Enter the substitute(s) (3 or less): \n")
-    return substitutions
-
-
-def getFileName(path):
-    """
-    Returns the file name.
-    """
-    return os.path.basename(path)
-
-
-def searchAndReplaceInFiles(keyWordSubstitutions, baseDirectory, directoryExclusions):
-    """
-    Searches and replaces the key words with the substitutes in the files.
-    """
-    for root, dirs, files in os.walk(baseDirectory, topdown=True, followlinks=False):
-        for directory in directoryExclusions:
-            if directory in dirs:
-                audit(f'Skipping excluded directory: {directory}')
-                dirs.remove(directory)
-        for file in files:
-            if getFileName(file) in common_excluded_files:
-                audit(f'Skipping excluded file: {file}')
-                continue
-            else:
-                filePath = os.path.join(root, file)
-                audit(f'Checking file: {filePath}')
-                # try to read file, skip if not readable
-                try:
-                    count = 0
-                    global totalReplacementCount
-                    with fileinput.FileInput(filePath, inplace=True, backup='.bak') as f:
-                        for line in f:
-                            rep = replaceText(line, keyWordSubstitutions)
-                            count += rep[1]
-                            print(rep[0], end='')
-                    audit(f'{count} replacements made')
-                    totalReplacementCount += count
-                except Exception as readError:
-                    audit(f'Skipping file, unreadable: {filePath}')
-                    audit(f'Exception: {formatErrorWithTrace(readError)}')
-                    continue
-                writeAuditsToFile()
-
-
-def replaceText(text, keyWordSubstitutions):
-    """
-    Replaces the key words with the substitutes in the text.
-    """
-    count = 0
-    for (keyword, substitute) in keyWordSubstitutions:
-        (newText, qty) = re.subn(keyword, substitute, text)
-        count += qty
-        text = newText
-    return [text, count]
+    return os.path.islink(path)
 
 
 def checkIfPathContainsExcludedWords(path, excludedWords):
@@ -125,18 +106,35 @@ def checkIfPathContainsExcludedWords(path, excludedWords):
     return False
 
 
-def isSymLink(path):
-    """
-    Checks if the path is a symbolic link.
-    """
-    return os.path.islink(path)
-
-
 def currentDirectory():
     """
     Returns the current directory.
     """
     return os.getcwd()
+
+
+def getFileName(path):
+    """
+    Returns the file name.
+    """
+    return os.path.basename(path)
+
+
+def formatErrorWithTrace(e):
+    """
+    Formats the error with the trace.
+    """
+    return f'{e}\n{traceback.format_exc()}'
+
+
+# User Input functions
+
+def askForBaseDirectory():
+    """
+    Asks the user for the base directory.
+    """
+    baseDirectory = input("Enter the base directory: \n")
+    return baseDirectory
 
 
 def askForDirectoryExclusions():
@@ -147,18 +145,21 @@ def askForDirectoryExclusions():
     return directoryExclusions.split()
 
 
-def formatErrorWithTrace(e):
+def askForKeyWords():
     """
-    Formats the error with the trace.
+    Asks the user for the keyword.
     """
-    return f'{e}\n{traceback.format_exc()}'
+    keyword = input("Enter the keyword to replace: \n")
+    return keyword
 
-def patternsToRegex(patterns):
-    split = patterns.split()
+
+def askForSubstitutes():
     """
-    Converts the patterns to regex.
+    Asks the user for the substitute for the corresponding key word.
     """
-    return [re.compile(re.escape(s)) for s in split]
+    substitutions = input("Enter the substitute word: \n")
+    return substitutions
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -169,14 +170,11 @@ if __name__ == '__main__':
         excludeDirectories = askForDirectoryExclusions()
         excludeDirectories = excludeDirectories + common_excluded_paths
         audit(f'Excluded directories: {excludeDirectories}')
-        keyWords = askForKeyWords()
-        substitutes = askForSubstitutes().split()
-        keyWordSubstitutions = zip(patternsToRegex(keyWords), substitutes)
-        audit(f'Key words: {keyWords}')
-        audit(f'Substitutes: {substitutes}')
-        audit(f'Key Word Substitution Tuple: {keyWordSubstitutions}')
-        searchAndReplaceInFiles(keyWordSubstitutions, baseDirectory, excludeDirectories)
-        audit(f'Total replacements made: {totalReplacementCount}')
+        keyword = askForKeyWords()
+        substitute = askForSubstitutes()
+        audit(f'Key words: {keyword}')
+        audit(f'Substitutes: {substitute}')
+        searchAndReplaceInFiles(keyword, substitute, baseDirectory, excludeDirectories)
         writeAuditsToFile()
     except Exception as e:
         audit(f'Error: {formatErrorWithTrace(e)}')
